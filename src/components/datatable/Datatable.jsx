291819d9dfa406bc7b5a58f850d1195875b6fbb4
Stephen Bailly <stephen.bailly@gmail.com>
2022-04-29 13:55:50 +0200

    initial commit

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,51 @@
+import { DataGrid } from '@mui/x-data-grid';
+import { userColumns, userRows } from "../../dataTableSource";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./Datatable.scss";
+
+const Datatable = () => {
+    const [data, setData] = useState(userRows);
+
+    const handleDelete = (id) => {
+        setData(data.filter((item) => item.id !== id));
+    };
+
+    const actionColumn = [
+        {
+            field: "action", headerName: "Action", width: 200, renderCell: (params) => {
+                return (
+                    <div className="cellAction">
+                        <Link to="/users/test" style={{ textDecoration: "none" }}>
+                        <div className="viewButton">View</div>
+                        </Link>
+                        <Link to="/users" style={{ textDecoration: "none" }}>
+                        <div className="deleteButton" onClick={() => handleDelete(params.row.id)}>Delete</div>
+                        </Link>
+                    </div>
+                );
+            },
+        },
+    ];
+
+    return (
+        <div className='datatable'>
+            <div className="datatableTitle">
+                Add New User
+                <Link to="/users/new" className="link">
+                    Add New
+                </Link>
+            </div>
+            <DataGrid
+                className="datagrid"
+                rows={data}
+                columns={userColumns.concat(actionColumn)}
+                pageSize={20}
+                rowsPerPageOptions={[10]}
+                checkboxSelection
+            />
+        </div>
+    )
+}
+
+export default Datatable
